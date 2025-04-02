f32616cc063765bf9b322bbf35bfe541d9ee2379
anato <anato@10.172.125.30>
2025-04-02 11:19:13 +0200

    operation/debut de produit

---
+++ projet_info/src/main/java/com/example/Operation.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example;
+
+/**
+ *
+ * @author anato
+ */
+public class Operation {
+    
+    private String refOperation;
+    private String dOperation;
+    private String refEquipement;
+    private float dureeOperation;
+
+    public String getRefOperation() {
+        return refOperation;
+    }
+
+    public void setRefOperation(String refOperation) {
+        this.refOperation = refOperation;
+    }
+
+    public String getdOperation() {
+        return dOperation;
+    }
+
+    public void setdOperation(String dOperation) {
+        this.dOperation = dOperation;
+    }
+
+    public String getRefEquipement() {
+        return refEquipement;
+    }
+
+    public void setRefEquipement(String refEquipement) {
+        this.refEquipement = refEquipement;
+    }
+
+    public float getDureeOperation() {
+        return dureeOperation;
+    }
+
+    public void setDureeOperation(float dureeOperation) {
+        this.dureeOperation = dureeOperation;
+    }
+
+    public Operation(String refOperation, String dOperation, String refEquipement, float dureeOperation) {
+        this.refOperation = refOperation;
+        this.dOperation = dOperation;
+        this.refEquipement = refEquipement;
+        this.dureeOperation = dureeOperation;
+    }
+    
+    
+}
